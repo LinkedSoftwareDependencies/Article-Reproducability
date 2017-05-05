1031bf36adfbf967eb0f2c75fb4435d97d0252bd
Ruben Verborgh <ruben@verborgh.org>
2017-05-05 16:30:08 +0200

    Add reset stylesheet.

--- lib/markupfilter.rb
+++ lib/markupfilter.rb
@@ -1,5 +1,14 @@
 Nanoc::Filter.define(:markupfilter) do |content, params|
+  content = content.dup
+
+  # Move references into <main>
+  references = content[%r{<h2 id="references">.*?</dl>}m]
+  content[references] = ''
+  content['</main>'] = "<section>\n" + references + "\n</section>\n</main>"
+
   # Move IDs on headings to parent sections
-  content = content.gsub /<section>(\s*)(<h\d[^>]*)(\sid=[^\s>]+)/,
-                         '<section\3>\1\2'
+  content.gsub! /<section>(\s*)(<h\d[^>]*)(\sid=[^\s>]+)/,
+                '<section\3>\1\2'
+
+  content
 end
