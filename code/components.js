1388646e0ca2ce09f381375462683eed8fa82629
Travis <travis@travis-ci.org>
2017-05-12 06:34:26 +0000

    Update to f482666d17cd5305f3ac55105189c24a4f8e69d6.

--- code/components.js
+++ code/components.js
@@ -1,8 +1,5 @@
-const Loader = require('lsd-components').Loader;
-let loader = new Loader();
-loader.registerModuleResourcesUrl(
-    'http://example.org/module-n3.ttl')
+new require('lsd-components').Loader()
+    .registerModuleResourcesUrl('http://example.org/module-n3.ttl')
     .then(() =>loader.instantiateFromUrl(
-        'http://example.org/config-n3.ttl#myParser',
-        'http://example.org/config-n3.ttl'))
+        'http://example.org/config-n3.ttl#myParser', 'http://example.org/config-n3.ttl'))
     .then((parser) => { ... });
