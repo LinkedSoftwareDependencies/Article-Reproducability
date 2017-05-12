db2490530de42074588a35d64fffe27293e3d756
Travis <travis@travis-ci.org>
2017-05-12 07:54:18 +0000

    Update to f8fe25231a2b6812f0e3f5344012ac73352308ec.

--- code/components.js
+++ code/components.js
@@ -1,5 +1,4 @@
-new require('lsd-components').Loader()
-    .registerModuleResourcesUrl('http://example.org/module-n3.ttl')
-    .then(() =>loader.instantiateFromUrl(
-        'http://example.org/config-n3.ttl#myParser', 'http://example.org/config-n3.ttl'))
-    .then((parser) => { ... });
+const loader = new require('lsd-components').Loader();
+loader.registerModuleResourcesUrl('http://example.org/module-n3.ttl')
+loader.registerConfigUrl('http://example.org/config-n3.ttl')
+var myParser = loader.instantiate('http://example.org/config-n3.ttl#myParser');
