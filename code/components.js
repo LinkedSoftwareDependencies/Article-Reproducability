54fee1a9b80e23d995592b0fa20d12d83b3db538
Travis <travis@travis-ci.org>
2018-04-06 21:05:15 +0000

    Update to b92faba609c66e08826bfbe5af084adbb9d76ac9.

--- code/components.js
+++ code/components.js
@@ -1,6 +1,4 @@
 const Loader = require('componentsjs').Loader;
 
 const loader = new Loader();
-await loader.registerModuleResourcesUrl('http://example.org/my-ldf-server.ttl');
-const myServer = await loader.instantiateFromUrl(
-    'http://example.org/myServer', 'http://example.org/config-ldf.ttl');
+const myServer = await loader.instantiate('http://example.org/config.ttl#myServer');
